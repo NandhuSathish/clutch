07b149c99b37e4ff47edea78a4f1fd9c04a65898
inv-nandhusathish <nandhu.sathish@innovaturelabs.com>
2023-09-19 14:32:47 +0530

    platformIcons added.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,21 @@
-import { Text } from '@chakra-ui/react';
-import useGames from '../hooks/useGames';
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import useGames, { Game } from '../hooks/useGames';
+import GameCard from './GameCard';
 
 const GameGrid = () => {
   const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      {games.length > 0 && (
-        <ul>
-          {games.map((game) => (
-            <li key={game.id}>{game.name}</li>
-          ))}
-        </ul>
-      )}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={3}
+        padding={'10px'}
+      >
+        {games.map((game: Game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
     </>
   );
 };
---
+++ src/hooks/Util.ts
@@ -0,0 +1,18 @@
+// export default function isValidUser() {
+//   const username = localStorage.getItem('username');
+//   if (username !== '') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// const isValidUser = ()=>{
+//   const username = localStorage.getItem('username');
+//     if (username !== '') {
+//       return true;
+//     } else {
+//       return false;
+//     }
+// }
+// export default  isValidUser;
